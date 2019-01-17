0de6f89ecce2fb6ce0a52591974cbfb5e6550d81
Eric Gurney <gurneysoftware@gmail.com>
2019-01-16 20:41:08 -0500

    initial commit

---
+++ StringKata.UnitTests/StringCalcTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringKata.UnitTests
+{
+    [TestFixture]
+    public class StringCalcTests
+    {
+        [Test]
+        public void EmptyParameter_Should_Return_0()
+        {
+            var result = StringCalculator.Calculate("");
+            result.Should().Be(0);
+        }
+
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("50", ExpectedResult = 50)]
+        public int StringOf1_ShouldReturn_Number(string numbers)
+        {
+            var result = StringCalculator.Calculate(numbers);
+            return result;
+        }
+
+        [TestCase("1,2", ExpectedResult = 3)]
+        public int StringOf2_ShouldReturn_Sum(string numbers)
+        {
+            var result = StringCalculator.Calculate(numbers);
+            return result;
+        }
+
+        [Test]
+        public void Should_UseNewLines_AsDelimiters()
+        {
+            var result = StringCalculator.Calculate("1\n2,3");
+            result.Should().Be(6);
+        }
+
+        [Test]
+        public void HandleUserDefinedDelimiter()
+        {
+            var result = StringCalculator.Calculate("//;\n1;2");
+            result.Should().Be(3);
+        }
+
+        [Test]
+        public void NegativeNumbersShouldThrowException()
+        {
+            Action act = () => StringCalculator.Calculate("1,-2");
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("whatever");
+        }
+
+        [TestCase("1,1001", ExpectedResult = 1)]
+        [TestCase("1,1000", ExpectedResult = 1001)]
+        public int IgnoreNumbersGreaterThan1000(string numbers)
+        {
+            var result = StringCalculator.Calculate(numbers);
+            return result;
+        }
+    }
+}
---
+++ StringKata/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringKata
+{
+    public static class StringCalculator
+    {
+        public static int Calculate(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var (modifiedNumbers, delimiters) = CreateDelimiters(numbers);
+            return CalculateSum(modifiedNumbers, delimiters);
+
+        }
+
+        private static int CalculateSum(string modifiedNumbers, List<char> delimiters)
+        {
+            var enumerable = modifiedNumbers
+                .Split(delimiters.ToArray())
+                .Select(int.Parse).Where(n => n <= 1000)
+                .ToList();
+
+            if (enumerable.Any(i => i < 0))
+                throw new InvalidOperationException("whatever");
+            return enumerable
+                .Sum();
+        }
+
+        private static (string, List<char>) CreateDelimiters(string numbers)
+        {
+            var delimiters = new List<char> {',', '\n'};
+
+            var regEx = new Regex(@"//(.*)\n");
+            if (regEx.IsMatch(numbers))
+            {
+                var matches = regEx.Match(numbers);
+                var delimiter = matches.Groups[1];
+                delimiters.Add(delimiter.Value[0]);
+                var valueLength = matches.Groups[0].Value.Length;
+                numbers = numbers.Substring(valueLength, numbers.Length - valueLength);
+            }
+
+            return (numbers, delimiters);
+        }
+    }
+}
